ac4d4ba140681a98c2f4b1164d484e8e1843063a
Admin <getmanartem@gmail.com>
2017-12-19 12:55:18 +0300

    GUI fixed

--- web-service-utils/web-service-utils/RestClient.swift
+++ web-service-utils/web-service-utils/RestClient.swift
@@ -12,12 +12,13 @@ class RestClient {
     var dataTask: URLSessionDataTask?
     
     func executeRequest(sqlRequest: String) -> String? {
-        let request = URLRequest(url: NSURL(string: sqlRequest)! as URL)
+        let sqlRequestSpaceReplaced = sqlRequest.replacingOccurrences(of: " ", with: "%20")
+        let request = URLRequest(url: NSURL(string: sqlRequestSpaceReplaced)! as URL)
         // Perform the request
         var response: AutoreleasingUnsafeMutablePointer<URLResponse?>? = nil
         let data = try! NSURLConnection.sendSynchronousRequest(request, returning: response)
         let responseData = String(data: data, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue))
-        print("result : \(responseData)")
+        print("result : \(responseData!)")
         return responseData
     }
 }
--- web-service-utils/web-service-utils/SQLRequestViewController.swift
+++ web-service-utils/web-service-utils/SQLRequestViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 import Foundation
 
 class SQLRequestViewController: UIViewController {
-    
+    @IBOutlet weak var txtServerPort: UITextField!
+    @IBOutlet weak var txtServerHost: UITextField!
     @IBOutlet weak var txtRequest: UITextView!
     @IBOutlet weak var responceTxt: UITextView!
     @IBAction func btnExecuteSqlRequest(_ sender: UIButton) {
         var client = RestClient()
         //let responce = client.executeRequest(sqlRequest: "http://193.124.113.157:8080/")
-        let responce = client.executeRequest(sqlRequest: txtRequest.text)
+        let url = "http://" + txtServerHost.text! + ":" + txtServerPort.text! + "/" + txtRequest.text!
+        print("request is: \(url)")
+        let responce = client.executeRequest(sqlRequest: url)
         print("responce is: \(responce!)")
         responceTxt.text!=responce!
     }
